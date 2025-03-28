5b324437771da7bba4de043373d7d20137fc2273
Markel Neila <markel.neila.2004@gmail.com>
2025-03-28 08:53:40 +0100

    Servicio1

--- src/app/services/servicio.fase1.service.ts
+++ src/app/services/servicio.fase1.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioFase1Service {
-
+  private http = inject(HttpClient)
+  apiURL ="https://chatbot-normativa-laboral.azurewebsites.net"
+  apiKey = ""
   constructor() { }
+  postQuestion(question: string){
+    const url = '${this.apiURL}';
+    const body = {
+      "question" : "",
+    }
+    return this.http.post(url, body);
+  }
 
 }
