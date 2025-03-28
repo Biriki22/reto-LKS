609869ea0a41509222394f8ed4bfc4651e4144ce
Markel Neila <markel.neila.2004@gmail.com>
2025-03-28 10:32:17 +0100

    Lateral texto y boton

---
+++ src/app/components/sidebar/historial-antiguo.component/historial-antiguo.component.component.ts
@@ -0,0 +1,19 @@
+import {Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+
+interface MenuOption {
+  icon: string;
+  label:string;
+  route: string;
+  subLabel:string;
+
+}
+@Component({
+  selector: 'historial-antiguo',
+  imports: [],
+  templateUrl: './historial-antiguo.component.component.html',
+})
+export class HistorialAntiguoComponentComponent {
+}
---
+++ src/app/components/sidebar/side-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { HistorialAntiguoComponentComponent } from "./historial-antiguo.component/historial-antiguo.component.component";
+
+@Component({
+  selector: 'gif-side-menu',
+  imports: [HistorialAntiguoComponentComponent],
+  templateUrl: './side-menu.component.html',
+})
+export class SideMenuComponent { }
