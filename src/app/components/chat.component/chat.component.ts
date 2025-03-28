de55e4d83c13bc048ed30c30bbcd59636fc5cb2a
Markel Neila <markel.neila.2004@gmail.com>
2025-03-28 12:35:13 +0100

    arreglao v7

--- src/app/components/chat.component/chat.component.ts
+++ src/app/components/chat.component/chat.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { NgClass, CommonModule } from '@angular/common';
-import { ChatConversationComponent } from "../chat-conversation/chat-conversation.component";
-=======
-import { ChatboxComponent } from '../../pages/chatbox/chatbox.component';
 import { TextoComponentComponent } from "../texto.component/texto.component.component";
+import { CommonModule, NgClass } from '@angular/common';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 
->>>>>>> 599c644e4290a5e5b57c92fda40b951c5a58d6a4
 
 interface ChatMessage {
   text: string;
@@ -17,11 +12,7 @@ interface ChatMessage {
 
 @Component({
   selector: 'app-chat',
-<<<<<<< HEAD
-  imports: [NgClass, CommonModule, ChatConversationComponent],
-=======
-  imports: [NgClass, CommonModule, TextoComponentComponent],
->>>>>>> 599c644e4290a5e5b57c92fda40b951c5a58d6a4
+  imports: [NgClass, CommonModule, TextoComponentComponent,HttpClientModule],
   templateUrl: './chat.component.html',
 })
 export class ChatComponent {
@@ -31,17 +22,18 @@ export class ChatComponent {
   constructor(private http: HttpClient) {}
 
   handleUserMessage(userText: string) {
-    // Agregar mensaje del usuario
-    this.messages.push({ text: userText, sender: 'user', timestamp: new Date() });
+        // Agregar mensaje del usuario
+        this.messages.push({ text: userText, sender: 'user', timestamp: new Date() });
 
-    // Enviar la consulta a la API
-    this.http.post<{ respuesta: string }>(this.apiUrl, { mensaje: userText }).subscribe(
-      (response) => {
-        this.messages.push({ text: response.respuesta, sender: 'bot', timestamp: new Date() });
-      },
-      (error) => {
-        this.messages.push({ text: 'Error al obtener respuesta del bot 😞', sender: 'bot', timestamp: new Date() });
+        // Enviar la consulta a la API
+        this.http.post<{ respuesta: string }>(this.apiUrl, { mensaje: userText }).subscribe(
+          (response) => {
+            this.messages.push({ text: response.respuesta, sender: 'bot', timestamp: new Date() });
+          },
+          (error) => {
+            this.messages.push({ text: 'Error al obtener respuesta del bot 😞', sender: 'bot', timestamp: new Date() });
+          }
+        );
       }
-    );
-  }
 }
+
