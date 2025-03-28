ac0a06b6ada0fb9c914894f72f43d424654a3dbd
Markel Neila <markel.neila.2004@gmail.com>
2025-03-28 11:19:15 +0100

    Areglao v3

--- src/app/pages/chatbox/chatbox.component.ts
+++ src/app/pages/chatbox/chatbox.component.ts
@@ -4,11 +4,12 @@ import { ChatComponent } from "../../components/chat.component/chat.component"
 import { SideMenuComponent } from '../../components/sidebar/side-menu.component';
 import { HistorialAntiguoComponentComponent } from '../../components/sidebar/historial-antiguo.component/historial-antiguo.component.component';
 import { RouterOutlet } from '@angular/router';
+import { ChatConversationComponent } from "../../components/chat-conversation/chat-conversation.component";
 
 
 @Component({
   selector: 'app-chatbox',
-  imports: [TextoComponentComponent, HistorialAntiguoComponentComponent, RouterOutlet, SideMenuComponent],
+  imports: [TextoComponentComponent, HistorialAntiguoComponentComponent, RouterOutlet, SideMenuComponent, ChatConversationComponent, ChatComponent],
 
   templateUrl: './chatbox.component.html',
 })
