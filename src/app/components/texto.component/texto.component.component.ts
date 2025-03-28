aac2343d7a5ba119456d377b8d2105b0266d64cb
Markel Neila <markel.neila.2004@gmail.com>
2025-03-28 09:26:07 +0100

    ts

--- src/app/components/texto.component/texto.component.component.ts
+++ src/app/components/texto.component/texto.component.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {Component } from '@angular/core';
 
 @Component({
   selector: 'app-texto.component',
   imports: [],
   templateUrl: './texto.component.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TextoComponentComponent { }
