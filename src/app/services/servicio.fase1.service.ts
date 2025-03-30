38a4cde687ca122ee3cc5e7a66a720474483037b
Markel <markel.neila.2004@gmail.com>
2025-03-30 03:02:57 +0200

    Comentarios

--- src/app/services/servicio.fase1.service.ts
+++ src/app/services/servicio.fase1.service.ts
@@ -5,15 +5,17 @@ import { inject, Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ServicioFase1Service {
   private http = inject(HttpClient)
-  apiURL ="https://chatbot-normativa-laboral.azurewebsites.net/Chat/Enviar"
+  apiURL ="https://chatbot-normativa-laboral.azurewebsites.net/Chat/Enviar" //Url de nuestra API
   apiKey = ""
   constructor() { }
 
   postQuestion(question: string){
     const url = `${this.apiURL}`;
-    const body = {question}
+    const body = {question} //definimos el body que tiene nuestra API en este caso una pregunta
     return this.http.post(url, body);
   }
 
