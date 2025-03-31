59ff4bfe0bc6017572fd5007c8f965f07ea396bf
Udaeta <julen.udaeta00@somo.eus>
2025-03-31 12:54:00 +0200

    historial sin localstorage

--- src/app/components/sidebar/side-menu.component.ts
+++ src/app/components/sidebar/side-menu.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { ServicioFase2Service } from '../../services/servicio.fase2.service';
+import { NgIf, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-side-menu',
+  standalone: true, // Es un componente standalone
   templateUrl: './side-menu.component.html',
+  imports: [ FormsModule, CommonModule] // Agrega FormsModule aquí
 })
 export class SideMenuComponent {
   newTitle: string = '';
--- src/app/pages/chatbox/chatbox.component.ts
+++ src/app/pages/chatbox/chatbox.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { TextoComponentComponent } from "../../components/texto.component/texto.component.component";
 import { ChatComponent } from "../../components/chat.component/chat.component"
 import { SideMenuComponent } from '../../components/sidebar/side-menu.component';
-import { HistorialAntiguoComponentComponent } from '../../components/sidebar/historial-antiguo.component/historial-antiguo.component.component';
+
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'chatbox',
-  imports: [HistorialAntiguoComponentComponent, RouterOutlet, SideMenuComponent, ChatComponent],
+  imports: [ RouterOutlet, SideMenuComponent, ChatComponent],
 
   templateUrl: './chatbox.component.html',
 })
